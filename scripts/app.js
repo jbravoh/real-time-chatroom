cf56239badd47f3a89d80bb91ba08a67e80e3dbd
jbravoh <jbravoharriott@gmail.com>
2022-03-09 21:22:20 +0000

    Send new chats and reset input field

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 // dom queries
 const chatList = document.querySelector('.chat-list')
+const newChatForm = document.querySelector('.new-chat')
 
+// add a new chat
+newChatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim()
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(error => console.log(error))
+
+})
 
 // class instances 
 const chatUI = new ChatUI(chatList);
@@ -9,3 +19,4 @@ const chatroom = new Chatroom('general', 'olivia');
 
 // get chats and render
 chatroom.getChats((data) => chatUI.render(data));
+
